494bfd0c750ff5b352a5128f379a370ec383ae97
ANDREA-ROCIO-PEREZ-ARRIOLA <pa25001@esfe.agape.edu.sv>
2025-09-12 15:08:03 -0600

    Se creo el metodo del login en el UsuarioEndpoints para configurar la creación del token

--- API_RyDBodegaAutenticacion/Endpoints/UsuarioEndpoints.cs
+++ API_RyDBodegaAutenticacion/Endpoints/UsuarioEndpoints.cs
@@ -1,7 +1,11 @@
 ﻿using API_RyDBodegaAutenticacion.DTOs;
 using API_RyDBodegaAutenticacion.Services.Rol;
 using API_RyDBodegaAutenticacion.Services.Usuario;
+using Microsoft.IdentityModel.Tokens;
 using Microsoft.OpenApi.Models;
+using System.IdentityModel.Tokens.Jwt;
+using System.Security.Claims;
+using System.Text;
 
 namespace API_RyDBodegaAutenticacion.Endpoints
 {
@@ -83,6 +87,48 @@ namespace API_RyDBodegaAutenticacion.Endpoints
             {
                 Summary = "Eliminar usuario",
                 Description = "Elimina un usuario existente"
+            });
+
+
+            group.MapPost("/login", async (CredencialesRequest credenciales, IUsuarioService usuarioService, IConfiguration config) =>
+            {
+                var login = await usuarioService.Login(credenciales);
+
+                if (login is null)
+                    //codigo 401 Unauthorized "No autorizado"
+                    return Results.Unauthorized();
+                else {
+                    var jwtSettings = config.GetSection("JwtSetting");
+                    var secretKey = jwtSettings.GetValue<string>("SecretKey");
+                    var issuer = jwtSettings.GetValue<string>("Issuer");
+                    var audience = jwtSettings.GetValue<string>("Audience");
+
+                    var tokenHandler = new JwtSecurityTokenHandler();
+                    var key = Encoding.UTF8.GetBytes(secretKey);
+
+                    var tokenDescriptor = new SecurityTokenDescriptor
+                    {
+                        Subject = new ClaimsIdentity(new[]
+                       {
+                            new Claim(ClaimTypes.Name, credenciales.Username)
+                       }),
+                       Expires = DateTime.UtcNow.AddHours(1),
+                       Issuer = issuer,
+                       Audience = audience,
+                       SigningCredentials = new SigningCredentials(new SymmetricSecurityKey(key), SecurityAlgorithms.HmacSha256Signature)
+                    };
+
+                    //Crear el token usando aparámetros definidos
+                    var token = tokenHandler.CreateToken(tokenDescriptor);
+                    //Convertir el token a string
+                    var jwt = tokenHandler.WriteToken(token);
+
+                    return Results.Ok(jwt);
+                }
+            }).WithOpenApi(o => new OpenApiOperation(o)
+            {
+                Summary = "Iniciar sesión",
+                Description = "Autentica a un usuario y devuelve un token JWT"
             });
 
         }
